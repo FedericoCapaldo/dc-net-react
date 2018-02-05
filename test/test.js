893f3a5822fff3b39b5c170751c65e982d14a2f0
Federico <federico.capaldo1@gmail.com>
2018-02-05 12:09:04 +0000

    checking if results of rounds are received in more elegant way

--- test/test.js
+++ test/test.js
@@ -6,16 +6,32 @@ let chatHelpers = require('../lib/chat-server');
 
 describe('Helper functions', function() {
   describe('Random name generator', function() {
-    it('Returns true if name is present in array', function() {
+    it('should return true if name is present in array', function() {
       let clients = [{name: "Alice", id: "xiu67324f"},
                  {name: "Bob", id: "chrqe9712bn"}];
       expect(chatHelpers.isNameUsed(clients, "Bob")).to.equal(true);
     });
 
-    it('Returns false if name is not present in array', function() {
+    it('should return false if name is not present in array', function() {
       let clients = [{name: "Alice", id: "xiu67324f"},
                  {name: "Bob", id: "chrqe9712bn"}];
       expect(chatHelpers.isNameUsed(clients, "Charlie")).to.equal(false);
     });
+  });
+
+  describe('Check All Round Results are present', function() {
+    it('should return false if some values are not received', function() {
+      let clients = [{name: "Alice", id: "xiu67324f", roundResult: 1},
+                     {name: "Bob", id: "chrqe9712bn", roundResult: -1},
+                     {name: "Charlie", id: "chrqe9712bn", roundResult: 0}];
+      expect(chatHelpers.areRoundsResultsReceived(clients)).to.equal(false);
+    });
+
+    it('should return true if all values are received on the server', function() {
+      let clients = [{name: "Alice", id: "xiu67324f", roundResult: 1},
+                     {name: "Bob", id: "chrqe9712bn", roundResult: 0},
+                     {name: "Charlie", id: "chrqe9712bn", roundResult: 0}];
+      expect(chatHelpers.areRoundsResultsReceived(clients)).to.equal(true);
+    })
   });
 });

46df50cf5afb1fa2b477700cba4354407a7937c3
Federico Capaldo <federico.capaldo1@gmail.com>
2018-04-15 02:47:09 +0100

    changed broadcastEvent of connecting client. deleted a couple of unused functions

--- server.js
+++ server.js
@@ -56,8 +56,6 @@ app.get('*', function(req, res) {
 });
 
 app.use((err, req, res, next) => {
-  console.error("Error on request %s %s", req.method, req.url);
-  console.error(err.stack);
   res.status(500).send("Server error");
 });
 

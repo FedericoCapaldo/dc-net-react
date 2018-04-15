ab6af8b7fcdc90754979db7ca60f408179a78a50
Federico Capaldo <federico.capaldo1@gmail.com>
2018-04-15 02:31:00 +0100

    refereing to correct stylesheet

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ function requireHTTPS(req, res, next) {
   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
     return res.redirect('https://' + req.get('host') + req.url);
   }
+  // allow next incoming requests
   next();
 }
 
